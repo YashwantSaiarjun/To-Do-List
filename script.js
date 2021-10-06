a6f3130c86ae4e61a1b212580fe21b86da085a48
Yashwant Saiarjun.S.V <86396701+YashwantSaiarjun@users.noreply.github.com>
2021-10-06 13:51:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let addToDobutton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('InputField');
+
+addToDobutton.addEventListener('click',function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText= inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = " ";
+
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+
+    paragraph.addEventListener('dblclick',function(){
+        toDoContainer.removeChild(paragraph);
+    })
+
+
+
+})
